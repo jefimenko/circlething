db9a5c3315aa86e8cbb04b2a864e05cd41799c22
Mattt Lee <mythicalphantasm@gmail.com>
2025-01-28 22:40:33 -0600

    Update test_import.py
    Updaing function call in test.

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,6 +2,6 @@ from a_module import a_py_file
 
 
 def test_my_foo():
-    my_bar = a_py_file.foo()
+    my_bar = a_py_file.i_pity_duh_foo()
 
-    assert my_bar is 'bar'
+    assert my_bar is "bar"
