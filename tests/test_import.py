439bec1138b59d5c44d8124812e2af5239841264
Mattt Lee <mythicalphantasm@gmail.com>
2025-01-28 22:45:14 -0600

    Updating to use relative import for function under test.

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,10 +2,13 @@
 This be how we drive development with tests. Kind of.
 """
 
-from a_module import a_py_file
+from ..a_module import a_py_file
 
 
 def test_my_foo():
+    """
+    Why do tests need docstrings? liek wut?
+    """
     my_bar = a_py_file.i_pity_duh_foo()
 
     assert my_bar is "bar"
